fae5143b76081c219957256f2de55fe2258d47ee
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2020-05-05 21:15:23 -0300

    End of Class#4 - List Rendering using v-for
    Moreover,
    :key is a special key attribute and is highly recommended to be used when
    using v-for to render lists because this way Vue can keep tracking each node's
    identity

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ var app = new Vue({
   data: {
     product: 'Socks',
     image: './assets/vmSocks-green-onWhite.jpg',
-    inventory: 2
+    inventory: 2,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue"
+      }
+    ]
   }
 })
