a17926d4512730021b1e95d319e899d8a47a6d89
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2020-05-09 19:14:17 -0300

    End of Class#11 - Use of Mounted property, eventBus and first talk about Vuex
    Moreover,
    - EventBus is a global channel to send information through Vue Components.
    - MOUNTED properties are called once the component is mounted in the DOM.
    - eventBus$on('event-to-be-listened', receivedParameter) works as a listener.
    - For global applications, to share Components States, it's recommended
    to use Vuex (a state management pattern + library for Vue.js applications).
    It serves as a centralized STORE for all the components in an application,
    with rules ensuring that the state can only be mutated in a predictable fashion.

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+var eventBus = new Vue()
+
 Vue.component('product', {
   props: {
     premium: {
@@ -31,19 +33,7 @@ Vue.component('product', {
         :class="{disabledButton: !inStock}"
         >Add to Cart</button>
     </div>
-    <div>
-      <h2>Reviews</h2>
-      <p v-show="!reviews.length">There are no reviews yet</p>
-      <ul>
-        <li v-for="review in reviews">
-        <p>{{review.name}}</p>
-        <p>Rating: {{review.rating}}</p>
-        <p>{{review.review}}</p>
-        </li>
-      </ul>
-    </div>
-
-    <product-review @review-submitted="addReview"></product-review>
+    <product-tabs :reviews="reviews"></product-tabs>
   </div>
   `,
   data() {
@@ -77,9 +67,6 @@ Vue.component('product', {
     },
     updateProduct(index) {
       this.selectedVariant = index
-    },
-    addReview(productReview) {
-      this.reviews.push(productReview)
     }
   },
   computed: {
@@ -99,6 +86,11 @@ Vue.component('product', {
       }
       return 2.99
     }
+  },
+  mounted() {
+    eventBus.$on('review-submitted', productReview => {
+      this.reviews.push(productReview)
+    })
   }
 })
 
@@ -151,7 +143,7 @@ Vue.component('product-review', {
           review: this.review,
           rating: this.rating
         }
-        this.$emit('review-submitted', productReview)
+        eventBus.$emit('review-submitted', productReview)
         this.name = null
         this.review = null
         this.rating = null
@@ -161,6 +153,42 @@ Vue.component('product-review', {
         if(!this.review) this.errors.push("Review is required!")
         if(!this.rating) this.errors.push("Rating is required!")
       }
+    }
+  }
+})
+
+Vue.component('product-tabs', {
+  props: {
+    reviews: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+    <div>
+      <span class="tab"
+        :class="{activeTab: selectedTab === tab}"
+        v-for="(tab, index) in tabs" :key="index"
+        @click="selectedTab = tab">{{tab}}
+      </span>
+      <div v-show="selectedTab === 'Reviews'">
+      <p v-show="!reviews.length">There are no reviews yet</p>
+      <ul>
+        <li v-for="review in reviews">
+        <p>{{review.name}}</p>
+        <p>Rating: {{review.rating}}</p>
+        <p>{{review.review}}</p>
+        </li>
+      </ul>
+    </div>
+
+    <product-review v-show="selectedTab === 'Make a Review'"></product-review>
+    </div>
+  `,
+  data() {
+    return {
+      tabs: ['Reviews', 'Make a Review'],
+      selectedTab: 'Reviews'
     }
   }
 })
